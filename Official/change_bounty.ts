378e2a03c10c6fd92126c026c53eec3e8e36fd1e
sa-shiro <snygglove_mira@outlook.com>
2020-02-26 01:17:26 +0100

    code cleanup, some additions

--- Official/change_bounty.ts
+++ Official/change_bounty.ts
@@ -6,76 +6,84 @@ var BountyMobs = [
 
 // function to activate an command block on x, y, z to execute a "/butcher 25 all" command to replace all visual bunties
 // so we wont have millions of entities. WARNING !! this will happen within the same tick !!!
-function changeBounty(args: fe.CommandArgs) {
+export function changeBounty(args: fe.CommandArgs) {
     var x = -407, y = 61, z = 398;
     var hiddenChatSender = args.sender.doAs(null, true);
 
-    Server.runCommand(hiddenChatSender, 'setblock', x, y, z  + ' redstone_block');
-    Server.runCommand(hiddenChatSender, 'setblock', x, y, z  + ' air');
-    setBounty(args);
+    Server.runCommand(hiddenChatSender, 'setblock', x, y, z + ' redstone_block');
+    Server.runCommand(hiddenChatSender, 'setblock', x, y, z + ' air');
+
+    // this function will be executed AFTER "/butcher 25 all" otherwise you will
+    // end up having tons of entities => timer is set to 3.000ms = 3 seconds
+    setTimeout(function () {
+        setBounty(args);
+    }, 3000);
 }
 
 // Function to refresh our (daily) Bounty
 export function setBounty(args: fe.CommandArgs) {
     // adding 3 visual bounties and display a chat message
-    // add or remove rN and bN to add or remove bounties (N = any number like r1, b15 etc)
-    
-    // Random 1,2,3: Gets an random Entity from our array and stores it in r1, r2 & r3.
-    var r1 = Math.floor((Math.random() * BountyMobs.length));
-    var r2 = Math.floor((Math.random() * BountyMobs.length));
-    var r3 = Math.floor((Math.random() * BountyMobs.length));
-    // Bounty 1,2,3: Generates a random float with an precision of 2 and stores it in b1,b2 & b3 => Used as our Bounty multiplier.
-    // Example: b1 = 3.76 means the players are getting 3.76 Coins per XP orb.
-    var b1 = (Math.random() * (4 - 0.50) + 0.50).toFixed(2); // (MAX - MIN) + MIN
-    var b2 = (Math.random() * (4 - 0.50) + 0.50).toFixed(2);
-    var b3 = (Math.random() * (4 - 0.50) + 0.50).toFixed(2);
+    // add or remove rN and bN to add or remove bounties (N = any number like rand1, mod15 etc)
+
+    // Random 1,2,3: Gets an random Entity from our array and stores it in rand1, rand2 & rand3.
+    var rand1 = Math.floor((Math.random() * BountyMobs.length));
+    var rand2 = Math.floor((Math.random() * BountyMobs.length));
+    var rand3 = Math.floor((Math.random() * BountyMobs.length));
+    // Bounty 1,2,3: Generates a random float with an precision of 2 and stores it in mod1,mod2 & mod3 => Used as our Bounty multiplier.
+    // Example: mod1 = 3.76 means the players are getting 3.76 Coins per XP orb.
+    var mod1 = (Math.random() * (2.5 - 0.50) + 0.50).toFixed(2); // (MAX - MIN) + MIN
+    var mod2 = (Math.random() * (2.5 - 0.50) + 0.50).toFixed(2);
+    var mod3 = (Math.random() * (2.5 - 0.50) + 0.50).toFixed(2);
     // "Player" used to execute the command.
     var hiddenChatSender = args.sender.doAs(null, true);
     // for loop variable
-    var i;
+    var i: int;
 
     // this function will reset ALL bounties
-    for(i = 0; i < BountyMobs.length; i++) {
+    for (i = 0; i < BountyMobs.length; i++) {
         Server.runCommand(hiddenChatSender, 'p', 'group Hunter clear', BountyMobs[i]);
     }
+    var x1 = -397, y1 = 65, z1 = 404; // Pos of Bounty Entity 1
+    var x2 = -405, y2 = 65, z2 = 404; // Pos of Bounty Entity 2
+    var x3 = -413, y3 = 65, z3 = 404; // Pos of Bounty Entity 3
+    var yn = 64, zn1 = 400;
+    var xn2 = -405, xn3 = -413;
 
-    // this function will be executed AFTER "/butcher 25 all" otherwise you will
-    // end up having tons of entities
-    // timer is set to 3.000ms = 3 seconds
-    setTimeout(function() {
-        var x1 = -397, y1 = 65, z1 = 404; // Pos of Bounty Entity 1
-        var x2 = -405, y2 = 65, z2 = 404; // Pos of Bounty Entity 2
-        var x3 = -413, y3 = 65, z3 = 404; // Pos of Bounty Entity 3
+    // seperating "fe.economy.bounty." from our array and stores it in name1.
+    var name1 = BountyMobs[rand1].slice(18, 60);
+    // Setting the bounty value.
+    Server.runCommand(hiddenChatSender, 'p', 'group Hunter value', BountyMobs[rand1], mod1);
+    // Announcing the Bounty.
+    Server.chat('§eDaily Bounty I: §4' + name1 + ' §e' + mod1 + ' Coins.');
+    // Spawning an Entity to display the current Bounty visually with its registry name.
+    // NBT Properties are:
+    // NoAI = dumb Entity, Silent = no noise, Invulnerable = cant be killed by Players
+    // NoGravity = makes the Entity fly
+    // CustomName = The display name above the Entity, CustomNameVisible = enables the CustomName || name1 = displaying the registryName of the Entity
+    // ActiveEffects: Fire resistance for Mobs that would burn on DayTime
+    Server.runCommand(hiddenChatSender, 'summon', name1, x1, y1, z1, '{NoAI:1, Silent:1, Invulnerable:1, NoGravity:1, CustomName:"",CustomNameVisible:1, ActiveEffects:[{Id:12,Duration:1000000,ShowParticles:0b}], Rotation:[180f,0f], PersistenceRequired:1}');
+    // displaying registryName and Bounty as floating text (set it to armorstand and add Invisible:1 to NBT to make it invis)
+    Server.runCommand(hiddenChatSender, 'summon', 'rabbit', x1, yn - 1, zn1, '{NoAI:1, Silent:1, Invulnerable:1, NoGravity:1, CustomName:"' + name1 + '", CustomNameVisible:1, ActiveEffects:[{Id:12,Duration:1000000,ShowParticles:0b}], Rotation:[180f,0f], PersistenceRequired:1}');
+    Server.runCommand(hiddenChatSender, 'summon', 'rabbit', x1, yn, zn1, '{NoAI:1, Silent:1, Invulnerable:1, NoGravity:1, CustomName:"§6' + mod1 + ' Coins", CustomNameVisible:1, ActiveEffects:[{Id:12,Duration:1000000,ShowParticles:0b}], Rotation:[180f,0f], PersistenceRequired:1}');
 
-        // seperating "fe.economy.bounty." from our array and stores it in n1.
-        var n1 = BountyMobs[r1].slice(18, 60);
-        // Setting the bounty value.
-        Server.runCommand(hiddenChatSender, 'p', 'group Hunter value', BountyMobs[r1], b1);
-        // Announcing the Bounty.
-        Server.chat('§eDaily Bounty I: §4' + n1 + ' §e' + b1 + ' Coins.');
-        // Spawning an Entity to display the current Bounty visually with its registry name.
-        // NBT Properties are:
-        // NoAI = dumb Entity, Silent = no noise, Invulnerable = cant be killed by Players
-        // NoGravity = makes the Entity fly, Invisible = unnecessary, but makes your Entity invisible if set to 1
-        // CustomName = The display name above the Entity, CustomNameVisible = enables the CustomName || n1 = displaying the registryName of the Entity
-        // ActiveEffects: Fire resistance for Mobs that would burn on DayTime
-        Server.runCommand(hiddenChatSender, 'summon', n1, x1, y1, z1, '{NoAI:1, Silent:1, Invulnerable:1, NoGravity:1, Invisible:0, CustomName:"', n1, '",CustomNameVisible:1, ActiveEffects:[{Id:12,Duration:1000000,ShowParticles:0b}], Rotation:[180f,0f]}');
+    var name2 = BountyMobs[rand2].slice(18, 60);
+    Server.runCommand(hiddenChatSender, 'p', 'group Hunter value', BountyMobs[rand2], mod2);
+    Server.chat('§eDaily Bounty II: §4' + name2 + ' §e' + mod2 + ' Coins.');
+    Server.runCommand(hiddenChatSender, 'summon', name2, x2, y2, z2, '{NoAI:1, Silent:1, Invulnerable:1, NoGravity:1, CustomName:"",CustomNameVisible:1, ActiveEffects:[{Id:12,Duration:1000000,ShowParticles:0b}], Rotation:[180f,0f], PersistenceRequired:1}');
+    Server.runCommand(hiddenChatSender, 'summon', 'rabbit', xn2, yn, zn1, '{NoAI:1, Silent:1, Invulnerable:1, NoGravity:1, CustomName:"§6' + mod2 + ' Coins", CustomNameVisible:1, ActiveEffects:[{Id:12,Duration:1000000,ShowParticles:0b}], Rotation:[180f,0f], PersistenceRequired:1}');
+    Server.runCommand(hiddenChatSender, 'summon', 'rabbit', xn2, yn - 1, zn1, '{NoAI:1, Silent:1, Invulnerable:1, NoGravity:1, CustomName:"' + name2 + '", CustomNameVisible:1, ActiveEffects:[{Id:12,Duration:1000000,ShowParticles:0b}], Rotation:[180f,0f], PersistenceRequired:1}');
 
-        var n2 = BountyMobs[r2].slice(18, 60);
-        Server.runCommand(hiddenChatSender, 'p', 'group Hunter value', BountyMobs[r2], b2);
-        Server.chat('§eDaily Bounty II: §4' + n2 + ' §e' + b2 + ' Coins.');
-        Server.runCommand(hiddenChatSender, 'summon', n2, x2, y2, z2, '{NoAI:1, Silent:1, Invulnerable:1, NoGravity:1, Invisible:0, CustomName:"', n2, '",CustomNameVisible:1, ActiveEffects:[{Id:12,Duration:1000000,ShowParticles:0b}], Rotation:[180f,0f]}');
-
-        var n3 = BountyMobs[r3].slice(18, 60);
-        Server.runCommand(hiddenChatSender, 'p', 'group Hunter value', BountyMobs[r3], b3);
-        Server.chat('§eDaily Bounty II: §4' + n3 + ' §e' + b3 + ' Coins.');
-        Server.runCommand(hiddenChatSender, 'summon', n3, x3, y3, z3, '{NoAI:1, Silent:1, Invulnerable:1, NoGravity:1, Invisible:0, CustomName:"', n3, '",CustomNameVisible:1, ActiveEffects:[{Id:12,Duration:1000000,ShowParticles:0b}], Rotation:[180f,0f]}');
-    }, 3000); // timer
+    var name3 = BountyMobs[rand3].slice(18, 60);
+    Server.runCommand(hiddenChatSender, 'p', 'group Hunter value', BountyMobs[rand3], mod3);
+    Server.chat('§eDaily Bounty III: §4' + name3 + ' §e' + mod3 + ' Coins.');
+    Server.runCommand(hiddenChatSender, 'summon', name3, x3, y3, z3, '{NoAI:1, Silent:1, Invulnerable:1, NoGravity:1, CustomName:"",CustomNameVisible:1, ActiveEffects:[{Id:12,Duration:1000000,ShowParticles:0b}], Rotation:[180f,0f], PersistenceRequired:1}');
+    Server.runCommand(hiddenChatSender, 'summon', 'rabbit', xn3, yn, zn1, '{NoAI:1, Silent:1, Invulnerable:1, NoGravity:1, CustomName:"§6' + mod3 + ' Coins", CustomNameVisible:1, ActiveEffects:[{Id:12,Duration:1000000,ShowParticles:0b}], Rotation:[180f,0f], PersistenceRequired:1}');
+    Server.runCommand(hiddenChatSender, 'summon', 'rabbit', xn3, yn - 1, zn1, '{NoAI:1, Silent:1, Invulnerable:1, NoGravity:1, CustomName:"' + name3 + '", CustomNameVisible:1, ActiveEffects:[{Id:12,Duration:1000000,ShowParticles:0b}], Rotation:[180f,0f], PersistenceRequired:1}');
 }
 
 FEServer.registerCommand({
-    name: 'changeBounty', // command thats typed into the chat 
-    usage: '/changeBounty',
+    name: 'bounty', // command thats typed into the chat 
+    usage: '/bounty',
     opOnly: true,
     processCommand: changeBounty
 });
